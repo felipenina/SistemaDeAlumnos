27e3635fe1de0af031e1a71bc585b81bddff58e4
felipenina <fnina2645@gmail.com>
2026-09-16 18:54:33 -0300

    Etapa 4

--- SistemaDeAlumnos/Alumno.cs
+++ SistemaDeAlumnos/Alumno.cs
@@ -15,4 +15,25 @@ public class Alumno
         Nota1 = nota1;
         Nota2 = nota2;
     }
+   
+    public double Promedio()
+    {
+        return (Nota1 + Nota2) / 2.0;
+    }
+
+    
+    public bool EstaAprobado()
+    {
+        return Promedio() >= 6.0;
+    }
+
+    public void SubirNota()
+    {
+        Nota1 = Math.Min(10.0, Nota1 + 1.0);
+        Nota2 = Math.Min(10.0, Nota2 + 1.0);
+    }
+    public override string ToString()
+    {
+        return $"{Legajo} - {Nombre} (promedio: {Promedio()})";
+    }
 }
--- SistemaDeAlumnos/Program.cs
+++ SistemaDeAlumnos/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using GestionAlumnos;
+Alumno alumno = new Alumno("Ana Pérez", 1234, 7.0, 7.0);
+
+Console.WriteLine(alumno);
